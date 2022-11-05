ee3f5a521a28ae6cde2771ca5cb3e209c679f50e
ququzone <xueping.yang@gmail.com>
2022-11-05 14:44:34 +0800

    add candigate query api

--- main.go
+++ main.go
@@ -94,7 +94,7 @@ func main() {
 	// log.Println(bucket.Owner)
 	// log.Printf("bucket #%d staking %s IOTX\n", bucket.Index, bucket.StakedAmount)
 
-	fmt.Println(fetchTotalStakingAmount(c))
+	fmt.Println(fetchCandidateByAddress(c, "io13eslm0ae6mdrj2uz7c260aj670wkdywtaye3gk"))
 }
 
 func createBucket(c iotex.AuthedClient, candidateName string) (uint64, error) {
@@ -203,6 +203,74 @@ func unstakeBucket(c iotex.AuthedClient, bucketId uint64) error {
 		return errors.New("action revert")
 	}
 	return nil
+}
+
+func fetchCandidateByName(c iotex.AuthedClient, name string) (string, error) {
+	method := &iotexapi.ReadStakingDataMethod{
+		Method: iotexapi.ReadStakingDataMethod_CANDIDATE_BY_NAME,
+	}
+	methodBytes, err := proto.Marshal(method)
+	if err != nil {
+		return "", err
+	}
+	argumentsBytes, err := buildReadCandidateByNameData(name)
+	if err != nil {
+		return "", err
+	}
+	response, err := c.API().ReadState(context.Background(), &iotexapi.ReadStateRequest{
+		ProtocolID: []byte("staking"),
+		MethodName: methodBytes,
+		Arguments:  [][]byte{argumentsBytes},
+		Height:     "",
+	})
+	if err != nil {
+		return "", err
+	}
+
+	var cand iotextypes.CandidateV2
+	err = proto.Unmarshal(response.Data, &cand)
+	if err != nil {
+		return "", err
+	}
+
+	if &cand == nil {
+		return "", errors.New("can't found candidate")
+	}
+	return cand.OwnerAddress, nil
+}
+
+func fetchCandidateByAddress(c iotex.AuthedClient, address string) (string, error) {
+	method := &iotexapi.ReadStakingDataMethod{
+		Method: iotexapi.ReadStakingDataMethod_CANDIDATE_BY_ADDRESS,
+	}
+	methodBytes, err := proto.Marshal(method)
+	if err != nil {
+		return "", err
+	}
+	argumentsBytes, err := buildReadCandidateByAddressData(address)
+	if err != nil {
+		return "", err
+	}
+	response, err := c.API().ReadState(context.Background(), &iotexapi.ReadStateRequest{
+		ProtocolID: []byte("staking"),
+		MethodName: methodBytes,
+		Arguments:  [][]byte{argumentsBytes},
+		Height:     "",
+	})
+	if err != nil {
+		return "", err
+	}
+
+	var cand iotextypes.CandidateV2
+	err = proto.Unmarshal(response.Data, &cand)
+	if err != nil {
+		return "", err
+	}
+
+	if &cand == nil {
+		return "", errors.New("can't found candidate")
+	}
+	return cand.OwnerAddress, nil
 }
 
 func fetchCandidates(c iotex.AuthedClient) ([]string, error) {
@@ -335,6 +403,28 @@ func buildReadCandidatesData(offset, limit uint32) ([]byte, error) {
 					Offset: offset,
 					Limit:  limit,
 				},
+			},
+		},
+	}
+	return proto.Marshal(arguments)
+}
+
+func buildReadCandidateByNameData(name string) ([]byte, error) {
+	arguments := &iotexapi.ReadStakingDataRequest{
+		Request: &iotexapi.ReadStakingDataRequest_CandidateByName_{
+			CandidateByName: &iotexapi.ReadStakingDataRequest_CandidateByName{
+				CandName: name,
+			},
+		},
+	}
+	return proto.Marshal(arguments)
+}
+
+func buildReadCandidateByAddressData(address string) ([]byte, error) {
+	arguments := &iotexapi.ReadStakingDataRequest{
+		Request: &iotexapi.ReadStakingDataRequest_CandidateByAddress_{
+			CandidateByAddress: &iotexapi.ReadStakingDataRequest_CandidateByAddress{
+				OwnerAddr: address,
 			},
 		},
 	}
